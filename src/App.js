e71a7db7179dc2a21995384145d84c8c8e34fc9a
Sam Broner <sam.broner@gmail.com>
2021-06-08 13:26:27 -0700

    Build our "serviceConfig"

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@
 import React from "react";
 import { SharedMap } from "@fluidframework/map";
 
+/**
+ * This function imitates a file system. It allows us to create durable IDs for containers so we can access them later.
+ * The ID is the number after the hash in the URL.
+ * In a production system, we may store the list of container ID's in another database.
+ */
+const getContainerId = () => {
+    let isNew = false;
+    if (window.location.hash.length === 0) {
+        isNew = true;
+        window.location.hash = Date.now().toString();
+    }
+    const containerId = window.location.hash.substring(1);
+    return { containerId, isdNew };
+};
+
+const { containerId, isNew } = getContainerId();
+const serviceConfig = { id: containerId };
+
 const containerSchema = {
     name: 'cra-demo-container',
     initialObjects: { mySharedMap: SharedMap }
