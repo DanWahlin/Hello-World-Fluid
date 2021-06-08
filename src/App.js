6f590ad1204ab0e8ec0564c3fa1739d4466d8d0c
Dan Wahlin <you@example.com>
2021-06-08 18:09:11 -0400

    Remove code and steps doc

--- src/App.js
+++ src/App.js
@@ -4,69 +4,22 @@
  */
 
 import React from "react";
-import { SharedMap } from "@fluidframework/map";
-import TinyliciousClient from "@fluid-experimental/tinylicious-client";
 
-/**
- * This function imitates a file system. It allows us to create durable IDs for containers so we can access them later.
- * The ID is the number after the hash in the URL.
- * In a production system, we may store the list of container ID's in another database.
- */
-const getContainerId = () => {
-    let isNew = false;
-    if (window.location.hash.length === 0) {
-        isNew = true;
-        window.location.hash = Date.now().toString();
-    }
-    const containerId = window.location.hash.substring(1);
-    return { containerId, isNew };
-};
 
-const { containerId, isNew } = getContainerId();
-const serviceConfig = { id: containerId };
 
-const containerSchema = {
-    name: 'cra-demo-container',
-    initialObjects: { mySharedMap: SharedMap }
-};
 
-TinyliciousClient.init();
 
-const getFluidData = async () => {
 
-    const [fluidContainer, ] = isNew
-        ? await TinyliciousClient.createContainer(serviceConfig, containerSchema)
-        : await TinyliciousClient.getContainer(serviceConfig, containerSchema);
 
-    return fluidContainer.initialObjects;
-}
 
 function App() {
-    const [fluidData, setFluidData] = React.useState();
+    // Add hooks
 
-    React.useEffect(() => {
-        getFluidData().then(data => setFluidData(data))
-    }, []);
 
-    const [viewData, setViewData] = React.useState();
 
-    React.useEffect(() => {
-        if (!fluidData) return;
 
-        const { mySharedMap } = fluidData;
 
-        const syncView = () => setViewData({ time: mySharedMap.get("time") });
-        
-        // ensure sync runs at least once
-        syncView();
 
-        // update state each time our map changes
-        mySharedMap.on("valueChanged", syncView);
-        return () => { mySharedMap.off("valueChanged", syncView) }
-
-    }, [fluidData])
-
-    const setTime = () => fluidData.mySharedMap.set("time", Date.now().toString());
 
     return (
         <div>
